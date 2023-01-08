da86cfc8be4a67dc46b50f306c37702ea263af66
GuilhermeEduardo23 <guilhermeeduardo-17@outlook.com>
2023-01-08 14:09:58 -0300

    Sencond Commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,46 @@
-function calcular(n1, n2) {
-    n1 = parseFloat(document.getElementById("n1").value);
-    n2 = parseFloat(document.getElementById("n2").value);
-    informacao = document.getElementById("resultado");
-    selector = document.getElementById("selector").value;
+//Botão Calcular
+const calc = document.getElementById(`calculate`);
 
-    const numeroNaoInformado = n1 === `` || n2 === ``;
+function calcular() {
+    const n1 = parseFloat(document.getElementById(`n1`).value);
+    const n2 = parseFloat(document.getElementById(`n2`).value);
+    const selector = document.getElementById(`selector`).value;
+    const result = document.getElementById(`result`);
 
-    switch(selector) {
-        case '+':
-            calculo = (n1 + n2);
-            break;
-        case '-':
-            calculo = (n1 - n2);
-            break;
-        case '*':
-            calculo = (n1 * n2);
-            break;
-        case '/':
-            calculo = Math.round((n1 / n2));
-            if(Number.isNaN(calculo)) {
-                document.getElementById("resultado").innerHTML = "Insira um divisível válido!";
-            }
-            break;
-    }
-
-    document.getElementById("resultado").innerHTML = `Resultado = ${calculo}`;
+        switch(selector) {
+            case '+':
+                calculo = (n1 + n2).toFixed(2);
+                break;
+            case '-':
+                calculo = (n1 - n2).toFixed(2);
+                break;
+            case '*':
+                calculo = (n1 * n2).toFixed(2);
+                break;
+            case '/':
+                calculo = Math.round((n1 / n2)).toFixed(2);
+                if(Number.isNaN(calculo)) {
+                    document.getElementById("resultado").innerHTML = "Insira um divisível válido!";
+                }
+                break;
+        }
+        result.textContent = `Resultado = ${calculo}`;
 }
+
+calc.addEventListener(`click`, calcular);
+
+//Botão Limpar
+
+const clean = document.getElementById(`clean`);
+
+function limpar() {
+    let n1 = document.getElementById(`n1`);
+    let n2 = document.getElementById(`n2`);
+    let resultado = document.getElementById(`result`);
+
+    n1.value = ``;
+    n2.value = ``;
+    resultado.textContent = ``;
+}
+
+clean.addEventListener(`click`, limpar);
