d45e4c84f67f1b58f3952be2dd38f117b9dc1d6d
GuilhermeEduardo23 <guilhermeeduardo-17@outlook.com>
2023-03-26 23:11:08 -0300

    Reformulação da interface e estrutura interna do projeto

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 function calcular(n1, n2) {
-    n1 = parseFloat(document.getElementById("n1").value);
-    n2 = parseFloat(document.getElementById("n2").value);
-    selector = document.getElementById("selector").value;
+    n1 = parseFloat(document.querySelector("#n1").value);
+    n2 = parseFloat(document.querySelector("#n2").value);
+    selector = document.querySelector("#selector").value;
 
     switch(selector) {
         case '+':
@@ -9,17 +9,33 @@ function calcular(n1, n2) {
             break;
         case '-':
             calculo = (n1 - n2);
-            break; //Teste de commit
+            break;
         case '*':
             calculo = (n1 * n2);
             break;
         case '/':
             calculo = Math.round((n1 / n2));
             if(Number.isNaN(calculo)) {
-                document.getElementById("resultado").innerHTML = "Insira um divisível válido!";
+                document.querySelector("#resultado").innerText = "Insira um divisível válido!";
             }
             break;
     }
 
-    document.getElementById("resultado").innerHTML = `Resultado = ${calculo}`;
+    document.querySelector("#resultado").innerText = `Resultado = ${calculo}`;
 }
+
+window.addEventListener(`load`, () => {
+    const fundo = document.querySelector(`#imagem`);
+
+    fundo.setAttribute(`src`, `https://source.unsplash.com/2000x1300/?calculator`);
+
+    const calculadora = document.querySelector(`main`);
+
+    calculadora.classList.add(`active`);
+
+    const principal = document.querySelector(`body`);
+    principal.classList.add(`responsive`);
+
+    if(principal.classList.value === `responsive`)
+        fundo.setAttribute(`src`, `https://source.unsplash.com/375x667/?calculator`);
+});
